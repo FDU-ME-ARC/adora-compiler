da61b3aca8f1e8df99e537158135d7f28374ec35
jiahang lou <loujiahang@n019-119-032.y.picoheart.net>
2026-05-07 17:41:02 +0800

    add a new branch for building task schedule

--- build_tools/build_adora.sh
+++ build_tools/build_adora.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
 BUILD_DIR="${PROJECT_ROOT}/build"
 
 ### set LLVM_BUILD_DIR to your own llvm path
-LLVM_BUILD_DIR="/home/share/onnx-mlir/third_party/llvm-project-onnx/build/"
-LLVM_INSTALL_DIR="${LLVM_BUILD_DIR}"
+LLVM_BUILD_DIR="${LLVM_BUILD_DIR:-$HOME/CGRVOPT/llvm-project-onnx/build}"
+LLVM_INSTALL_DIR="${LLVM_INSTALL_DIR:-${LLVM_BUILD_DIR}}"
 
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR"
